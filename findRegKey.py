9c0cb0aefd576287f963dfb662a77ff8c111d109
i8vista <34743706+i8vista@users.noreply.github.com>
2018-06-19 17:25:14 +0800

    1

--- findRegKey.py
+++ findRegKey.py
@@ -1,4 +1,4 @@
-import winreg,re
+import winreg,re,pprint
 
 def findReg(key):
     try:
@@ -6,27 +6,32 @@ def findReg(key):
         while True:
             Name, Value, Type = winreg.EnumValue(key, i)
             if Type == 1:
-                if re.compile(r'NVIDIA').search(Value):
+                if re.compile(r'NVIDIA|Nv3D').search(Value):
                     return True
             i +=1
     except WindowsError:
         return False
 
+def text1(sub_key,reg_list = []):
+    def findkey():
+        nonlocal sub_key
+        nonlocal reg_list
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, sub_key,
+                            reserved=0, access=winreg.KEY_READ)
+        if findReg(key):
+            reg_list.append(re.compile(r'^(.*?\\.*?\\.*?\\)([^\\]*)').search(sub_key).group(2))
+        else:
+            try:
+                index = 0
+                parentPath = sub_key
+                while True:
+                    sub_key = parentPath + '\\' + winreg.EnumKey(key, index)
+                    findkey()
+                    index += 1
+            except WindowsError:
+                pass
+    return findkey
 
-def findkey(sub_key):
-    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, sub_key,
-                        reserved=0, access=winreg.KEY_READ)
-    if findReg(key):
-        print(sub_key)
-        return
-    try:
-        index = 0
-        parentPath = sub_key
-        while True:
-            sub_key = parentPath + '\\' + winreg.EnumKey(key, index)
-            findkey(sub_key)
-            index += 1
-    except WindowsError:
-        pass
-
-findkey(r'SOFTWARE\Classes\CLSID')
+mylist = []
+text1(r'SOFTWARE\Classes\CLSID',mylist)()
+pprint.pprint(set(mylist))
